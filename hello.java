9df4b4535e1315df6b4dbfdf02d54e299ca0e83a
John Funk <funkjo@my.easternct.edu>
2020-10-28 00:32:08 -0400

    changed value of firstName and updated print statement

--- hello.java
+++ hello.java
@@ -1,16 +1,15 @@
-// a simple program to practice common Github commands 
+// a simple program to practice common Github commands
 
 public class hello {
 
     public static void main(String [] args) {
 
         // initialize variables
-        String firstName = "Alex";
-	
+        String firstName = "John";
+
 		// output welcome message
-        System.out.println("Hello world!");
+        System.out.println("Hello my name is " + firstName);
 
 
     }
 }
-
